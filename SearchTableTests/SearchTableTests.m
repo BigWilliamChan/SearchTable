e47aaf7af83de256b013a68a1a9318014b3e4ef5
chenwei910101 <william@williamdeMacBook-Pro.local>
2014-02-23 20:19:54 +0800

    Initial Commit

---
+++ SearchTableTests/SearchTableTests.m
@@ -0,0 +1,34 @@
+//
+//  SearchTableTests.m
+//  SearchTableTests
+//
+//  Created by william on 14-2-23.
+//  Copyright (c) 2014年 william. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SearchTableTests : XCTestCase
+
+@end
+
+@implementation SearchTableTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
